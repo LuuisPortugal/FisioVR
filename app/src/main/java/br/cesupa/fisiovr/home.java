c05201f883bfc8b76bdde8b302ed955ee8708542
luis.portugal <luis.portugal@cinbesa.com.br>
2017-09-19 17:59:35 -0300

    login

--- app/src/main/java/br/cesupa/fisiovr/home.java
+++ app/src/main/java/br/cesupa/fisiovr/home.java
@@ -1,7 +1,20 @@
 package br.cesupa.fisiovr;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -11,10 +24,30 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.cesupa.fisiovr.adapter.SimpleItemRecyclerViewAdapter;
 import br.cesupa.fisiovr.list.FisioterapeutaListActivity;
@@ -22,15 +55,34 @@ import br.cesupa.fisiovr.list.PacienteListActivity;
 import br.cesupa.fisiovr.list.SessaoListActivity;
 import br.cesupa.fisiovr.list.VideoListActivity;
 
+import static android.Manifest.permission.READ_CONTACTS;
+
 public class home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private boolean mTwoPane;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private TextView mEmailView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    private View activity_login_include;
+    private View activity_item_list_include;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        mEmailView = (TextView) findViewById(R.id.email);
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+        activity_login_include = findViewById(R.id.activity_login_include);
+        activity_item_list_include = findViewById(R.id.activity_item_list_include);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -51,6 +103,185 @@ public class home extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                updateView(user);
+            }
+        };
+
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        updateView(mAuth.getCurrentUser());
+    }
+
+    //Atualiza as Views
+    private void updateView(FirebaseUser user){
+        if (user != null) {
+            activity_login_include.setVisibility(View.GONE);
+            activity_item_list_include.setVisibility(View.VISIBLE);
+        } else {
+            activity_login_include.setVisibility(View.VISIBLE);
+            activity_item_list_include.setVisibility(View.GONE);
+        }
+    }
+
+    //Executa a ação de ir para outra pagina
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        if(mAuth.getCurrentUser() != null) {
+            // Handle navigation view item clicks here.
+            int id = item.getItemId();
+
+            if (id == R.id.nav_sessao_iniciar) {
+                Toast.makeText(this, "nav_sessao_iniciar", Toast.LENGTH_SHORT).show();
+            } else if (id == R.id.nav_sessao_Lista) {
+                startActivity(new Intent(this, SessaoListActivity.class));
+            } else if (id == R.id.nav_videos_novos) {
+                Intent videoListIntent = new Intent(this, VideoListActivity.class);
+                videoListIntent.putExtra(VideoListActivity.TAG_VIDEOS_TYPE, VideoListActivity.NEW_VIDEOS_TYPE);
+
+                startActivity(videoListIntent);
+            } else if (id == R.id.nav_videos_salvos) {
+                Intent videoListIntent = new Intent(this, VideoListActivity.class);
+                videoListIntent.putExtra(VideoListActivity.TAG_VIDEOS_TYPE, VideoListActivity.SAVED_VIDEOS_TYPE);
+
+                startActivity(videoListIntent);
+            } else if (id == R.id.nav_pessoas_fisioterapeutas) {
+                startActivity(new Intent(this, FisioterapeutaListActivity.class));
+            } else if (id == R.id.nav_pessoas_pacientes) {
+                startActivity(new Intent(this, PacienteListActivity.class));
+            }
+        }else{
+            Toast.makeText(this, getString(R.string.error_non_logged), Toast.LENGTH_SHORT).show();
+            mEmailView.requestFocus();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //Validate Email
+    private boolean isEmailValid(String email) {
+        return email.contains("@");
+    }
+
+    //Validate Senha
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
+    }
+
+    //Loga
+    private void attemptLogin() {
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if(TextUtils.isEmpty(password)){
+            mPasswordView.setError(getString(R.string.error_field_required));
+            focusView = mPasswordView;
+            cancel = true;
+        }else if (!isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+            showProgress(true);
+            mAuth.signInWithEmailAndPassword(mEmailView.getText().toString(), mPasswordView.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        showProgress(false);
+                        if (!task.isSuccessful()) {
+                            mPasswordView.setError(task.getException().getMessage());
+                            mPasswordView.requestFocus();
+                        }
+                    }
+                });
+        }
+    }
+
+    //Mostra o carregando
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     @Override
@@ -79,39 +310,11 @@ public class home extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            if(mAuth != null){
+                mAuth.signOut();
+            }
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_sessao_iniciar) {
-            Toast.makeText(this, "nav_sessao_iniciar", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.nav_sessao_Lista) {
-            startActivity(new Intent(this, SessaoListActivity.class));
-        } else if (id == R.id.nav_videos_novos) {
-            Intent videoListIntent = new Intent(this, VideoListActivity.class);
-            videoListIntent.putExtra(VideoListActivity.TAG_VIDEOS_TYPE, VideoListActivity.NEW_VIDEOS_TYPE);
-
-            startActivity(videoListIntent);
-        } else if (id == R.id.nav_videos_salvos) {
-            Intent videoListIntent = new Intent(this, VideoListActivity.class);
-            videoListIntent.putExtra(VideoListActivity.TAG_VIDEOS_TYPE, VideoListActivity.SAVED_VIDEOS_TYPE);
-
-            startActivity(videoListIntent);
-        } else if (id == R.id.nav_pessoas_fisioterapeutas) {
-            startActivity(new Intent(this, FisioterapeutaListActivity.class));
-        } else if (id == R.id.nav_pessoas_pacientes) {
-            startActivity(new Intent(this, PacienteListActivity.class));
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
